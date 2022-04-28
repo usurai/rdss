fb3faa759d0840e64fe2d3800f50c61158ab9a82
usurai <crowdwei@gmail.com>
2022-04-29 03:05:07 +0800

    Read from incoming connection and print to stdout.

--- server.cc
+++ server.cc
@@ -6,17 +6,34 @@
 #include <cstring>
 #include <iostream>
 #include <liburing.h>
+#include <string_view>
+#include <vector>
 
 static constexpr size_t QD = 64;
+static constexpr size_t READ_SIZE = 4096;
 
 int sock;
 io_uring ring;
+
+struct Connection {
+    enum class State { Init, Reading, Writting };
+
+    State state{State::Init};
+    int fd;
+    std::vector<char> read_buffer;
+    size_t read_length{0};
+
+    Connection(int fd_)
+      : fd(fd_)
+      , read_buffer(READ_SIZE) {}
+};
 
 void queue_multishot_accept() {
     auto* sqe = io_uring_get_sqe(&ring);
     assert(sqe != nullptr);
     // TODO: use direct variant
     io_uring_prep_multishot_accept(sqe, sock, nullptr, nullptr, 0);
+    io_uring_sqe_set_data(sqe, nullptr);
     io_uring_submit(&ring);
 }
 
@@ -70,20 +87,42 @@ int main() {
             return 1;
         }
         if (cqe->res < 0) {
-            std::cerr << "async accept: " << strerror(errno) << '\n';
+            std::cerr << "async op: " << strerror(-cqe->res) << '\n';
             return 1;
         }
-        std::cout << "accepted\n";
-        close(cqe->res);
-        if (!(cqe->flags & IORING_CQE_F_MORE)) {
-            std::cout << "requeueing accept\n";
-            queue_multishot_accept();
+
+        // if accept
+        //   queue read
+        if (cqe->user_data == 0) {
+            if (!(cqe->flags & IORING_CQE_F_MORE)) {
+                std::cout << "requeueing accept\n";
+                queue_multishot_accept();
+            }
+
+            auto conn = new Connection(cqe->res);
+            auto* sqe = io_uring_get_sqe(&ring);
+            assert(sqe != nullptr);
+            io_uring_prep_read(sqe, cqe->res, conn->read_buffer.data(), READ_SIZE, 0);
+            io_uring_sqe_set_data(sqe, reinterpret_cast<void*>(conn));
+            io_uring_submit(&ring);
+        } else {
+            bool close_conn{false};
+
+            const auto read_len{cqe->res};
+            auto* conn = reinterpret_cast<Connection*>(cqe->user_data);
+            conn->state = Connection::State::Reading;
+            conn->read_length = read_len;
+            std::cout << "Read " << conn->read_length << ":"
+                      << std::string_view(conn->read_buffer.data(), conn->read_length);
+            close_conn = true;
+
+            if (close_conn) {
+                close(conn->fd);
+                delete conn;
+            }
         }
         io_uring_cqe_seen(&ring, cqe);
     }
-    //      if accept
-    //          queue accept
-    //          queue read
     //      if read
     //          if not read finish, queue read
     //          else process
