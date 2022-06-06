86c3491920ab45593b5d152cb1dddcb27c47b490
usurai <crowdwei@gmail.com>
2022-06-06 12:12:24 +0800

    Move gtest assert out of lambda.

--- test/client_test.cc
+++ test/client_test.cc
@@ -4,30 +4,52 @@
 
 #include "climits"
 
-using rdss::Client;
+namespace rdss::test {
 
-TEST(ClientTest, parseBuffer) {
+class ClientTest : public testing::Test {
+public:
+    static Client CreateClient(std::string buffer) {
     Client c(nullptr, 0);
+        c.query_buffer = std::move(buffer);
+        c.read_length = c.query_buffer.size();
+        return c;
+    }
 
-    auto assert_argc = [&](const std::string& buffer, size_t expected_argc) {
-        memset(c.query_buffer.data(), 0, c.query_buffer.size());
-        memcpy(c.query_buffer.data(), buffer.data(), buffer.size());
-        c.cursor = 0;
-        c.arguments.clear();
+    static bool CompareArgv(Client& c, std::vector<std::string> argvs = {}) {
+        if (c.arguments.size() != argvs.size()) {
+            return false;
+        }
+        for (size_t i = 0; i < c.arguments.size(); ++i) {
+            const auto& arg{c.arguments[i]};
+            if (std::string_view(arg.data, arg.length).compare(argvs[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+TEST(ClientTest, parseBufferArgc) {
+    auto get_argc = [&](const std::string& buffer) {
+        Client c(nullptr, 0);
+        c.query_buffer = buffer;
+        c.read_length = buffer.size();
         c.ParseBuffer();
-        EXPECT_EQ(expected_argc, c.arguments.size());
+        return c.arguments.size();
     };
 
-    assert_argc("", 0);
-    assert_argc("\r\n", 0);
-    assert_argc("*\r\n", 0);
-    assert_argc("*1\r\n", 1);
-    assert_argc("*12345\r\n", 12345);
+    EXPECT_EQ(get_argc(""), 0);
+    EXPECT_EQ(get_argc("\r\n"), 0);
+    EXPECT_EQ(get_argc("*\r\n"), 0);
+    EXPECT_EQ(get_argc("*1\r\n"), 1);
+    EXPECT_EQ(get_argc("*12345\r\n"), 12345);
     const auto int_max_str = "*" + std::to_string(INT_MAX) + "\r\n";
-    assert_argc(int_max_str, INT_MAX);
+    EXPECT_EQ(get_argc(int_max_str), INT_MAX);
     const auto overflow_str = "*" + std::to_string(static_cast<long long>(INT_MAX) + 1) + "\r\n";
-    assert_argc(overflow_str, 0);
-    assert_argc("*-1\r\n", 0);
-    assert_argc("*12345a\r\n", 0);
-    assert_argc("*x1\r\n", 0);
+    EXPECT_EQ(get_argc(overflow_str), 0);
+    EXPECT_EQ(get_argc("*-1\r\n"), 0);
+    EXPECT_EQ(get_argc("*12345a\r\n"), 0);
+    EXPECT_EQ(get_argc("*x1\r\n"), 0);
 }
+
+} // namespace rdss::test
