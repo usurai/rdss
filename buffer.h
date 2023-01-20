ccc079b98e1447f2ccea94b593d8a517d21f7f7b
usurai <crowdwei@gmail.com>
2023-01-20 18:14:49 +0800

    Better.

--- buffer.h
+++ buffer.h
@@ -4,9 +4,28 @@
 #include <cstddef>
 #include <string_view>
 #include <vector>
+#include <span>
 
 namespace rdss {
 
+/***
+Usage:
+1. Write to the buffer:
+    constexpr size_t BUF_SIZE = 1024;
+    Buffer buf(BUF_SIZE);
+    ...
+    buf.EnsureAvailable(BUF_SIZE);
+    auto nread = read(buf.Data(), buf.Available(), BUF_SIZE);
+    or
+    auto sink = buf.Sink();
+    auto nread = read(sink.data(), sink.size(), BUF_SIZE);
+    if (nread == 0) ...
+    else buf.CommitWrite(nread);
+2. Read from the buffer:
+    auto to_consume = buf.Stored();
+    auto bytes_consumed = process(to_consume);
+    buf.Consume(bytes_consumed);
+***/
 class Buffer {
 public:
     using BufferView = std::string_view;
@@ -14,26 +33,13 @@ public:
 public:
     virtual ~Buffer() = default;
 
-    /***
-    usage | write to buffer:
-    constexpr size_t BUF_SIZE = 1024;
-    Buffer buf(BUF_SIZE);
-    ...
-    buf.EnsureCapcacity(BUF_SIZE);
-    auto nread = read(buf.Data(), buf.Available(), BUF_SIZE);
-    if (nread == 0) ...
-    else buf.CommitWrite(nread);
-    ***/
     virtual void EnsureAvailable(size_t n) = 0;
     virtual char* Data() = 0;
     virtual size_t Available() const = 0;
+    virtual std::span<char> Sink() = 0;
     virtual void CommitWrite(size_t n) = 0;
 
     /***
-    usage | read from buffer:
-    auto to_consume = buf.Stored();
-    auto bytes_consumed = process(to_consume);
-    buf.Consume(bytes_consumed);
     ***/
     virtual size_t NumWritten() const = 0;
     virtual BufferView Stored() const = 0;
@@ -44,12 +50,13 @@ public:
 
 class VectorBuffer : public Buffer {
 public:
-    explicit VectorBuffer(size_t capacity)
-      : data_(capacity)
-      , read_index_(0)
-      , write_index_(0) {}
+    VectorBuffer() = default;
 
-    virtual ~VectorBuffer() = default;
+    explicit VectorBuffer(size_t capacity)
+      : data_(capacity) {}
+
+    // TODO: This is unnecessary.
+    // virtual ~VectorBuffer() = default;
 
     // TODO: Consider move forward if there is space at front. If resize, consider expand more.
     virtual void EnsureAvailable(size_t n) override {
@@ -62,6 +69,10 @@ public:
     virtual char* Data() override { return data_.data() + write_index_; }
 
     virtual size_t Available() const override { return write_index_ - read_index_; }
+
+    virtual std::span<char> Sink() override {
+        return std::span(data_.data()+write_index_, Available());
+    }
 
     virtual void CommitWrite(size_t n) override {
         assert(data_.size() >= write_index_ + n);
@@ -94,8 +105,8 @@ public:
 
 private:
     std::vector<char> data_;
-    size_t read_index_;
-    size_t write_index_;
+    size_t read_index_{0};
+    size_t write_index_{0};
 };
 
 } // namespace rdss
