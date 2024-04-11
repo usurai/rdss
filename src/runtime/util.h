3ea9bc4fb6706cdcb1c4c9d7dd01f35299ae194e
usurai <crowdwei@gmail.com>
2024-04-11 18:28:19 +0800

    chore: Remove unused ScheduleOn()
    Along with its dependency Awaitable.

--- src/runtime/util.h
+++ src/runtime/util.h
@@ -21,39 +21,9 @@ struct Timeout : public RingOperation<Timeout> {
     __kernel_timespec ts_;
 };
 
-struct Awaitable
-  : public Continuation
-  , public std::suspend_always {
-    void await_suspend(std::coroutine_handle<> h) {
-        handle = std::move(h);
-        io_uring_sqe* sqe;
-        while ((sqe = io_uring_get_sqe(ring)) == nullptr) {
-        }
-        io_uring_prep_msg_ring(sqe, to_fd, 0, reinterpret_cast<uint64_t>(this), 0);
-        io_uring_sqe_set_flags(sqe, IOSQE_CQE_SKIP_SUCCESS);
-        if (submit) {
-            auto ret = io_uring_submit(ring);
-            if (ret < 0) {
-                LOG(FATAL) << "io_uring_submit:" << strerror(-ret);
-            }
-        }
-    }
-
-    io_uring* ring;
-    int to_fd;
-    bool submit;
-};
-
 } // namespace rdss::detail
 
 namespace rdss {
-
-// TODO: Deprecate this after change to EchoServer.
-template<typename FuncType>
-Task<void> ScheduleOn(io_uring* src_ring, RingExecutor* dest_exr, FuncType func) {
-    co_await detail::Awaitable{.ring = src_ring, .to_fd = dest_exr->RingFD(), .submit = true};
-    func();
-}
 
 /// Sets up 'exrs' to init buffer rings. Blocking waits all of them to finish setup before return.
 void SetupInitBufRing(std::vector<std::unique_ptr<RingExecutor>>& exrs);
