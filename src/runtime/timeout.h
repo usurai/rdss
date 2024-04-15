0276f3d70852fb2cac2676e9f1f429f66545c1b4
usurai <crowdwei@gmail.com>
2024-04-16 00:49:41 +0800

    runtime: Move Timeout awaitable to its header

---
+++ src/runtime/timeout.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "runtime/ring_operation.h"
+
+namespace rdss {
+
+struct Timeout : public RingOperation<Timeout> {
+    // TODO: Support generalized duration.
+    Timeout(RingExecutor* executor, std::chrono::nanoseconds nanoseconds)
+      : RingOperation<Timeout>(executor)
+      , ts_{.tv_sec = 0, .tv_nsec = nanoseconds.count()} {}
+
+    void Prepare(io_uring_sqe* sqe) { io_uring_prep_timeout(sqe, &ts_, 0, 0); }
+
+    void await_resume() {
+        if (result != -ETIME && result != 0) {
+            LOG(FATAL) << "io_uring timeout: " << strerror(-result);
+        }
+    }
+
+    __kernel_timespec ts_;
+};
+
+} // namespace rdss
--- src/runtime/util.h
+++ src/runtime/util.h
@@ -1,27 +1,7 @@
 #pragma once
 
 #include "runtime/ring_operation.h"
-
-namespace rdss::detail {
-
-struct Timeout : public RingOperation<Timeout> {
-    // TODO: Support generalized duration.
-    Timeout(RingExecutor* executor, std::chrono::nanoseconds nanoseconds)
-      : RingOperation<Timeout>(executor)
-      , ts_{.tv_sec = 0, .tv_nsec = nanoseconds.count()} {}
-
-    void Prepare(io_uring_sqe* sqe) { io_uring_prep_timeout(sqe, &ts_, 0, 0); }
-
-    void await_resume() {
-        if (result != -ETIME && result != 0) {
-            LOG(FATAL) << "io_uring timeout: " << strerror(-result);
-        }
-    }
-
-    __kernel_timespec ts_;
-};
-
-} // namespace rdss::detail
+#include "runtime/timeout.h"
 
 namespace rdss {
 
@@ -29,7 +9,7 @@ namespace rdss {
 void SetupInitBufRing(std::vector<std::unique_ptr<RingExecutor>>& exrs);
 
 inline auto WaitFor(RingExecutor* exr, std::chrono::nanoseconds duration) {
-    return detail::Timeout(exr, duration);
+    return Timeout(exr, duration);
 }
 
 /// Returns an awaitable that suspends the execution of the current coroutine and resumes it on the
